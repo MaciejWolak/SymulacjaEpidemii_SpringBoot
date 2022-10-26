3081111772844165e987bc990c7017f0c9227689
Maciej Wolak <maciejwolak@gmail.com>
2022-10-26 10:23:26 +0200

    final2

--- src/main/java/com/covid19app/model/Results.java
+++ src/main/java/com/covid19app/model/Results.java
@@ -9,7 +9,7 @@ import javax.persistence.*;
 @Entity
 public class Results {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Double numberOfInfectedPeople;
     private Double numberOfHealthyPeople;
